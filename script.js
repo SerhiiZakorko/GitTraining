29cda94cad7e90ca31eaeca8c9901b802317e5cf
Zakorko Serhii <rkot.vet@gmail.com>
2023-06-29 09:41:30 +0200

    somewhat

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(8);
+
+console.log();
+
+getResult(18, 19);
+console.log(c);
+
+function getResult(a, b) {
+  return (c = a + b);
+}
+sayHi("Serhii");
+
+function sayHi(user_name) {
+  console.log(`hi ${user_name}!`);
+}
+
+power(11, 2);
+
+function power(a, b) {
+  console.log(Math.pow(a, b));
+}
